f3e2a2309a49c01ecd919aac43c51ef1ea288f2c
Poorna-Vaishnavi <131003457+Poorna-Vaishnavi@users.noreply.github.com>
2023-07-14 21:38:19 +0530

    Add files via upload

---
+++ sum_of_subarray.py
@@ -0,0 +1,18 @@
+def sum_of_subarray_sums(A):
+    N = len(A)
+    total_sum = 0
+
+    for i in range(N):
+        current_sum = 0
+        for j in range(i, N):
+            current_sum += A[j]
+            total_sum += current_sum
+
+    return total_sum
+
+A = input("Enter the elements of the array : ")
+A = list(map(int, A.split()))
+
+result = sum_of_subarray_sums(A)
+
+print("Sum of all subarray sums:", result)
